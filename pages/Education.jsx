30f58ff3e5d6c447fdc546d4372030baf02e6a28
rohitumadi <rohitumadi2000@gmail.com>
2024-01-29 20:56:35 +0530

    initial commit

---
+++ pages/Education.jsx
@@ -0,0 +1,5 @@
+function Education() {
+  return <div>Education page</div>;
+}
+
+export default Education;
---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import AppLayout from '../ui/AppLayout';
+import Projects from '../pages/Projects';
+import Education from '../pages/Education';
+import Contact from '../pages/Contact';
+import PageNotFound from '../pages/PageNotFound';
+import Homepage from '../pages/Homepage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate to="homepage" replace />} />
+          <Route path="homepage" element={<Homepage />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="education" element={<Education />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
